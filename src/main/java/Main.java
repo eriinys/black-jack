5a0b44d6920053066fe0f08f82489cf7abc3d3fd
eriinys <eriinys@duck.com>
2025-10-24 14:28:14 -0500

    setup Main class and prompt user's name and number of players

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Hand> players = new ArrayList<>();
+        ArrayList<String> defaultName = new ArrayList<>();
+        defaultName.add("player2");
+        defaultName.add("player3");
+        defaultName.add("player4");
+        defaultName.add("player5");
+        defaultName.add("player6");
+        defaultName.add("player7");
+
+        System.out.println("Welcome to BlackJack🃏\n" +
+                "Please enter your name: ");
+        String player = scanner.nextLine();
+        Hand player1 = new Hand(player);
+        players.add(player1);
+
+        System.out.println("Enter number of players:");
+        int numberOfPlayers = Integer.parseInt(scanner.nextLine());
+        for(int i = 0; i < numberOfPlayers - 1; i++){
+            String name = defaultName.get(i);
+            players.add(new Hand(name));
+        }
+
+
+
+
+    }
+
+
 }
