cbd4f35ffed0bd980766e247179ac1e1c863316a
eriinys <eriinys@duck.com>
2025-10-24 14:27:08 -0500

    set up Hand class

--- src/main/java/Hand.java
+++ src/main/java/Hand.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Hand {
+    private ArrayList<Card> cards;
+    private String playerName;
+
+    public Hand(String playerName){
+        this.playerName = playerName;
+        cards = new ArrayList<>();
+    }
+
+    public void deal(Card card){
+        cards.add(card);
+    }
+
+    public int getSize(){
+        return cards.size();
+    }
+
+    public int getValue(){
+        int value = 0;
+        for (Card card : cards){
+            card.flip();
+            value += card.getPointValue();
+            card.flip();
+        }
+        return value;
+    }
+
+    public String toString(){
+        return  String.format("%s", playerName);
+    }
 }
